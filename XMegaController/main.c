8e59402b2f4fe0c44c0639572fe12557db65d3bc
Pipei Sandor <sandor.pipei@arobs.com>
2016-06-24 10:51:54 +0300

    Added licence information in core files

--- XMegaController/main.c
+++ XMegaController/main.c
@@ -1,9 +1,14 @@
-/*
- * XMegaController.c
+/* ----------------------------------------------------------------------- *
  *
- * Created: 6/22/2016 21:06:21
- * Author : Pipei Sandor
- */ 
+ *   Copyright 2016 Pipei Sandor - All Rights Reserved
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, Inc., 675 Mass Ave, Cambridge MA 02139,
+ *   USA; either version 2 of the License, or (at your option) any later
+ *   version; incorporated herein by reference.
+ *
+ * ----------------------------------------------------------------------- */
 
  #include <avr/io.h>
  #include "FreeRTOS.h"
@@ -31,7 +36,7 @@
  {
 	 xTaskCreate(TestTaskFunction1, "Task_1",1000, NULL,1,NULL);
 	 xTaskCreate(TestTaskFunction2, "Task_2",1000, NULL,1,NULL);
-	 /* Replace with your application code */
+	 
 	 vTaskStartScheduler();
 	 while (1)
 	 {
